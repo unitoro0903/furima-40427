5928d0e5565a952a57689b63874ab1a1f65d81d8
unitoro0903 <koishi.koji2@gmail.com>
2024-07-17 01:10:47 +0900

    ストロングパラメーター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,3 +11,9 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name_kanji,:first_name_kanji,:family_name_kana,:first_name_kana,:birthday])
+  end
