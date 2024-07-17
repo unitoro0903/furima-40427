695147aa90187fb49d20beb19830827906671290
unitoro0903 <koishi.koji2@gmail.com>
2024-07-17 19:14:58 +0900

    対応するカラムの設定とモデルにバリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+ 
+  
   private
 
   def basic_auth
@@ -13,7 +15,6 @@ class ApplicationController < ActionController::Base
 end
 
 
-private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name_kanji,:first_name_kanji,:family_name_kana,:first_name_kana,:birthday])
   end
