224893b3d5479b91c23cc5db7ac54e51c4dc1dd9
unitoro0903 <koishi.koji2@gmail.com>
2024-07-20 21:10:33 +0900

    修正2
    レビューいただきありがとうございます。
    4点ほどわからない部分があり、先ほどのレビューに返信させていただいております。
    特にitemsのモデルを削除してしまうと最初のホームページが消えてしまうと思っています。

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,7 @@ class User < ApplicationRecord
          VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
          validates :family_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "はカタカナで入力して下さい。" }
          validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "はカタカナで入力して下さい。" }
-        #with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-          #validates :family_name_kana
-          #validates :first_name_kana
-       # end
+        
          
          validates :birthday, presence: true
          
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
+    password  { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     family_name_kanji {Faker::Name.last_name}
     first_name_kanji {Faker::Name.first_name}
