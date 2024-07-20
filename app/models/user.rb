27d6814a3f599f627fb9d654c999f639aea0f7d4
unitoro0903 <koishi.koji2@gmail.com>
2024-07-20 20:19:09 +0900

    修正1
    - [https://gyazo.com/5f735dc4ae7798888c64b1962d483e20] 入力に問題のある状態で「会員登録」ボタンが押された場合、ユーザーの新規登録はできず、エラーメッセージが出力される動画
    -  [https://gyazo.com/6393f0653de867026323323f8fe6ada9] テストに成功する画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+         validates :password, format: { with: /\A[ -~｡-ﾟ]*\z/, message: 'must not include full-width characters.' }
          validates :nickname, presence: true
-         validates :family_name_kanji, presence: true
-         validates :first_name_kanji, presence: true
-         validates :family_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          validates :family_name_kanji
+          validates :first_name_kanji
+         end
+         VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+         validates :family_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "はカタカナで入力して下さい。" }
+         validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "はカタカナで入力して下さい。" }
+        #with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          #validates :family_name_kana
+          #validates :first_name_kana
+       # end
+         
          validates :birthday, presence: true
          
-         has_many :items
-         has_many :records
+         #has_many :items
+         #has_many :records
          
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it "nicknameとemailとpasswordとpassword_confirmationとfamily_name_kanjiとfirst_name_kanjiとfamily_name_kanaとfirst_name_kanaとbirthdayが存在する" do
+        @user.family_name_kanji = "位"
+        @user.first_name_kanji = "位"
+        @user.family_name_kana = "ア"
+        @user.first_name_kana = "ア"
         expect(@user).to be_valid
       end
     end
@@ -76,6 +80,10 @@ RSpec.describe User, type: :model do
       end
 
         it '重複したemailが存在する場合は登録できない' do
+          @user.family_name_kanji = "位"
+         @user.first_name_kanji = "位"
+         @user.family_name_kana = "ア"
+         @user.first_name_kana = "ア"
           @user.save
           another_user = FactoryBot.build(:user, email: @user.email)
           another_user.valid?
@@ -87,6 +95,144 @@ RSpec.describe User, type: :model do
           @user.valid?
           expect(@user.errors.full_messages).to include('Email is invalid')
       end
+
+      it '英字のみのパスワードでは登録できない' do
+        @user.family_name_kana = "亜"
+        @user.first_name_kana = "亜"
+        @user.family_name_kana = "ア"
+        @user.first_name_kana = "ア"
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+
+        it '数字のみのパスワードでは登録できない' do
+          @user.family_name_kana = "亜"
+        @user.first_name_kana = "亜"
+        @user.family_name_kana = "ア"
+        @user.first_name_kana = "ア"
+          @user.password = '123456'
+        @user.password_confirmation = '123456'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+        end
+
+          it '全角文字を含むパスワードでは登録できない' do
+            @user.family_name_kana = "亜"
+            @user.first_name_kana = "亜"
+            @user.family_name_kana = "ア"
+            @user.first_name_kana = "ア"
+            @user.password = 'ｒuby123'
+          @user.password_confirmation = 'ｒuby123'
+            @user.valid?
+            expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers', 'Password must not include full-width characters.')
+          end
+
+            it '姓（全角）に半角文字が含まれていると登録できない' do
+            
+              @user.first_name_kanji = "亜"
+              @user.family_name_kana = "ア"
+              @user.first_name_kana = "ア"
+              @user.family_name_kanji = 'hankaku'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('Family name kanji 全角文字を使用してください')
+            end
+
+            it '名（全角）に半角文字が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              @user.family_name_kana = "ア"
+              @user.first_name_kana = "ア"
+              @user.first_name_kanji = 'hankaku'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('First name kanji 全角文字を使用してください')
+            end
+
+            it '姓（カナ）にカタカナ以外の平仮名が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              @user.first_name_kana = "ア"
+              @user.first_name_kanji = '亜'
+              @user.family_name_kana = 'ひらがな'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('Family name kana はカタカナで入力して下さい。')
+            end
+
+            it '名（カナ）にカタカナ以外の平仮名が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              @user.family_name_kana = "ア"
+             
+              @user.first_name_kanji = '亜'
+              @user.first_name_kana = 'ひらがな'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('First name kana はカタカナで入力して下さい。')
+            end
+
+            it '姓（カナ）にカタカナ以外の漢字が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              @user.first_name_kana = "ア"
+              @user.first_name_kanji = '亜'
+              @user.family_name_kana = '漢字'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('Family name kana はカタカナで入力して下さい。')
+            end
+
+            it '名（カナ）にカタカナ以外の漢字が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              @user.family_name_kana = "ア"
+              
+              @user.first_name_kanji = '亜'
+              @user.first_name_kana = '漢字'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('First name kana はカタカナで入力して下さい。')
+            end
+
+            it '姓（カナ）にカタカナ以外の英数字が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              
+              @user.first_name_kana = "ア"
+              @user.first_name_kanji = '亜'
+              @user.family_name_kana = 'アeisuuji1'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('Family name kana はカタカナで入力して下さい。')
+            end
+
+            it '名（カナ）にカタカナ以外の英数字が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              @user.family_name_kana = "ア"
+              
+              @user.first_name_kanji = '亜'
+              @user.first_name_kana = 'アeisuuji1'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('First name kana はカタカナで入力して下さい。')
+            end
+
+            it '姓（カナ）にカタカナ以外の記号が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              
+              @user.first_name_kana = "ア"
+              @user.first_name_kanji = '亜'
+              @user.family_name_kana = '＠'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('Family name kana はカタカナで入力して下さい。')
+            end
+
+            it '名（カナ）にカタカナ以外の記号が含まれていると登録できない' do
+              @user.family_name_kanji = "亜"
+              
+              @user.family_name_kana = "ア"
+              
+              @user.first_name_kanji = '亜'
+              @user.first_name_kana = '＠'
+              @user.valid?
+              expect(@user.errors.full_messages).to include('First name kana はカタカナで入力して下さい。')
+            end
+            
+            
     end
   end
 end
