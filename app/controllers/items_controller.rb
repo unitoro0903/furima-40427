0a9ade1220fdb9a4c6db366469dc19c882df52ef
unitoro0903 <koishi.koji2@gmail.com>
2024-07-21 01:58:24 +0900

    出品ボタンの画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
   def new
-    
-end
+    @item = Item.new
+  end
 
 def create
 
@@ -15,4 +16,6 @@ private
 
 def item_params
   params.require(:item).permit(:content, :item).merge(user_id: current_user.id)
+end
+
 end
