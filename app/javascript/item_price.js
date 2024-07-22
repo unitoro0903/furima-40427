0c848938f228ad1cf16986404133342da231ca31
unitoro0903 <koishi.koji2@gmail.com>
2024-07-22 16:49:54 +0900

    レビュー修正1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,8 @@
-window.addEventListener('load', function(){
+window.addEventListener('turbo:load', function(){
   document.getElementById("item-price").addEventListener('keyup', function(){
     let price = Number(document.getElementById("item-price").value);
     let fee = Math.floor(price * 0.1);
-    let profit = price - fee;
+    let profit = Math.floor(price - fee);
     document.getElementById("add-tax-price").textContent = fee;
     document.getElementById("profit").textContent = profit;
   })
