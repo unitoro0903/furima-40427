fe3887daa504885313c557ba35ea9cac8e666b2c
unitoro0903 <koishi.koji2@gmail.com>
2024-07-23 17:45:35 +0900

    商品情報編集機能
    - [https://gyazo.com/6c4ecadffc3776dc9cb3f03f38c9b259 ] ログイン状態の出品者は、商品情報編集ページに遷移できる動画
    - [https://gyazo.com/a72669c8bc392625b358f3e9717eccf0 ] 必要な情報を適切に入力して「更新する」ボタンを押すと、商品の情報を編集できる動画
    - [https://gyazo.com/3bcb39a32460dc1683a492847fd06a75] 入力に問題がある状態で「変更する」ボタンが押された場合、情報は保存されず、編集ページに戻りエラーメッセージが表示される動画
    - [https://gyazo.com/2e4deab5961205e7d846c3ad76e7ef56] 何も編集せずに「更新する」ボタンを押しても、画像無しの商品にならない動画
    - [https://gyazo.com/1c4b82a81c86f83b6221d6274857db37 ] ログイン状態の場合でも、URLを直接入力して自身が出品していない商品の商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずトップページに遷移する動画
    - [https://gyazo.com/29d5273a70691f98e4087f6782c53144 ] ログアウト状態の場合は、URLを直接入力して商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずログインページに遷移する動画
    - [ https://gyazo.com/6c4ecadffc3776dc9cb3f03f38c9b259] 商品名やカテゴリーの情報など、すでに登録されている商品情報は商品情報編集画面を開いた時点で表示される動画（商品画像・販売手数料・販売利益に関しては、表示されない状態で良い）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,6 +21,23 @@ end
 
 def show
   @item = Item.find(params[:id])
+end
+
+def edit
+  @item = Item.find(params[:id])
+  if @item.user_id != current_user.id
+    redirect_to root_path
+  end
+end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    p @item.errors.full_messages
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 private
