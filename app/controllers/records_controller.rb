f072c1dd03d68f5b973edcdfba494432346e7cbc
unitoro0903 <koishi.koji2@gmail.com>
2024-07-27 14:15:40 +0900

    クレジットカード決済機能
    クレジットカード決済機能の実装のために、PAYJP導入
    鍵はコードに書けないので、環境変数に設定

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,9 @@
 class RecordsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
+  before_action :redirect_if_owner_or_sold_out, only: :index
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new
   end
@@ -14,20 +16,41 @@ class RecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new(record_params)
     if @record_address.valid?
+      pay_item
       @record_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def current_item
+    @current_item ||= Item.find(params[:item_id])
   end
   end
 
  private
  def record_params
   # params.permit(item_id: current_item.id).merge(user_id: current_user.id)
-  params.require(:record_address).permit(:postal_code, :region_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+  params.require(:record_address).permit(:postal_code, :region_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: current_item.id,token: params[:token])
 end
 
+def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+    amount: current_item.price,  # 商品の値段
+    card: record_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end
+
+def redirect_if_owner_or_sold_out
+  @item = Item.find(params[:item_id])
+  if @item.sold_out? || current_user.id == @item.user_id
+    redirect_to root_path
+  end
+end
 # def address_params
   # params.permit(:postal_code, :region_id, :city, :house_number, :building_name, :phone_number).merge(record_id: @record.id)
 # end
