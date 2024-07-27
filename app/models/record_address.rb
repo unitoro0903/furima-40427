0683d2542b342501c787f2c363920fb2d8296b1a
unitoro0903 <koishi.koji2@gmail.com>
2024-07-27 20:54:48 +0900

    presence: trueの重複部分消す
    7行目の`presence: true`が適用されているため、不要な presence: trueは消しました
    
    アクティブハッシュのidを指定したいのでnumericality: に変更しました

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -9,12 +9,12 @@ class RecordAddress
     validates :user_id
     validates :item_id
     # validates :record_id
-    validates :token, presence: true
-    validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は「3桁ハイフン4桁」の半角文字列で入力してください" }
-    validates :region_id, presence: true
-    validates :city, presence: true
-    validates :house_number, presence: true
-    validates :phone_number, presence: true, length: { in: 10..11 }, format: { with: /\A[0-9]+\z/, message: "は半角数値のみ入力してください" }
+    validates :token
+    validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は「3桁ハイフン4桁」の半角文字列で入力してください" }
+    validates :region_id,  numericality: { other_than: 0 , message: "を選択してください" }
+    validates :city
+    validates :house_number
+    validates :phone_number, length: { in: 10..11 }, format: { with: /\A[0-9]+\z/, message: "は半角数値のみ入力してください" }
   end
   def save
   
